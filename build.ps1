8442c92faafee34c69368e2377c91415da276015
Xinrea <xinreas@outlook.com>
2024-08-07 15:21:33 +0800

    script: update build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,28 @@
-npm run build --prefix ./resources/panel
-cmake . -Bbuild
-cmake --build ./build --config Debug --target ALL_BUILD -j 26
-./build/bin/JPet/Debug/JPet.exe
+# 停止执行脚本在遇到错误时
+$ErrorActionPreference = 'Stop'
+
+try {
+    # 运行 npm 构建命令
+    Push-Location ./resources/panel
+    Write-Host "Running npm build ..."
+    npm run build
+    Pop-Location
+
+    # 配置 CMake 项目
+    Write-Host "Configuring CMake project ..."
+    cmake . -Bbuild
+
+    # 构建项目
+    Write-Host "Building project ..."
+    cmake --build ./build --config Debug --target ALL_BUILD -j 26
+
+    # 运行构建的程序
+    Write-Host "Running built application ..."
+    & "./build/bin/JPet/Debug/JPet.exe"
+
+} catch {
+    Write-Host "An error occurred: $_" -ForegroundColor Red
+    exit $_.Exception.HResult
+} finally {
+    Write-Host "Script completed." -ForegroundColor Green
+}
