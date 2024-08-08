dd6819b378b62905f459c669a6276b2724315368
Xinrea <xinreas@outlook.com>
2024-08-08 21:12:39 +0800

    script: update build script

--- build.ps1
+++ build.ps1
@@ -4,20 +4,20 @@ $ErrorActionPreference = 'Stop'
 try {
     # 运行 npm 构建命令
     Push-Location ./resources/panel
-    Write-Host "Running npm build ..."
+    Write-Host "Running npm build ..." -ForegroundColor Green
     npm run build
     Pop-Location
 
     # 配置 CMake 项目
-    Write-Host "Configuring CMake project ..."
+    Write-Host "Configuring CMake project ..." -ForegroundColor Green
     cmake . -Bbuild
 
     # 构建项目
-    Write-Host "Building project ..."
+    Write-Host "Building project ..." -ForegroundColor Green
     cmake --build ./build --config Debug --target ALL_BUILD -j 26
 
     # 运行构建的程序
-    Write-Host "Running built application ..."
+    Write-Host "Running built application ..." -ForegroundColor Green
     & "./build/bin/JPet/Debug/JPet.exe"
 
 } catch {
