f0d156829275fa136ad5663a3f3ab4b4cecc6a95
Xinrea <xinreas@outlook.com>
2024-08-28 09:56:28 +0800

    game: nerf starcnt

--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -269,7 +269,7 @@ int DataManager::CurrentExpDiff() {
   if (bf->IsBirthday()) {
     exp *= 6;
   }
-  exp *= (1 + 0.5 * starcnt);
+  exp *= (1 + 0.2 * starcnt);
   return std::min(exp, 99999999);
 }
 
